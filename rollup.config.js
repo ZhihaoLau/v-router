f8de45b4f4d311bc7a22d9d49de77dc2ba708af5
liuzhihao <lozio@live.cn>
2017-09-14 22:03:05 +0800

    babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,14 @@
+const babel = require('rollup-plugin-babel');
+import babelrc from 'babelrc-rollup';
+
 export default {
-    entry: `${__dirname}/src/index.js`,
+    input: `${__dirname}/src/index.js`,
     output: {
         file: `${__dirname}/dist/v-router.js`,
         format: 'umd',
         name: 'VRouter'
-    }
+    },
+    plugins: [
+        babel(babelrc())
+    ]
 };
