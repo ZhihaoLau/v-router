636b6e30480bb4a07f21e7b483afd1c09db45079
lozio <lozio@live.cn>
2017-09-20 10:06:49 +0800

    unit test

--- test/util.test.js
+++ test/util.test.js
@@ -1,15 +1,34 @@
-let assert = require('assert')
+import { expect } from 'chai'
 import * as util from '../src/util'
 
 describe('Util', () => {
-	describe('getUrlParams', () => {
-		it('should return object', () => {
-			let url = 'https://abc.com#abc'
+	describe('#getUrlParams', () => {
+		let longUrl = 'http://www.abc.com/#/blogs?q=es6&month=9'
 
-			assert.deepEqual(util.getUrlParams(url), {
-				path: 'abc',
-				query: {}
+		it('should return object', () => {
+			expect(util.getUrlParams(longUrl)).to.be.a('object')
+		})
+
+		it('should get hashName', () => {
+			expect(util.getUrlParams(longUrl).path).to.be.equal('/blogs')
+		})
+
+		it('should get query', () => {
+			expect(util.getUrlParams(longUrl).query).to.deep.equal({
+				month: '9',
+				q: 'es6'	
 			})
 		})
+
+		let shortUrl = 'http://www.abc.com'
+
+		it('should not get hashName while it not present', () => {
+			expect(util.getUrlParams(shortUrl).path).to.be.undefined
+		})
+
+		it('should return empty object while query not present', () => {
+			expect(util.getUrlParams(shortUrl).query).to.deep.equal({})
+		})
+
 	})	
 })
